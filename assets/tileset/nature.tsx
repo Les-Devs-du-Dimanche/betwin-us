a2c5641f4ec26c2cd2b1545d329003a72639b664
Elistellar <eliott.piccand@gmail.com>
2022-03-20 23:00:34 +0100

    merge

--- assets/tileset/nature.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.2" name="nature" tilewidth="64" tileheight="64" tilecount="288" columns="16">
- <image source="../pictures/nature.png" width="1024" height="1152"/>
-</tileset>
--- test.py
+++
@@ -1,5 +0,0 @@
-from src.generator.minimap import Minimap
-
-a = Minimap('map_5')
-print(a.door)
-
