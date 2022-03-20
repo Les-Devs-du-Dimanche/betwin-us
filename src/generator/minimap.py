f80f43131b90c0b144f957aa32f31577bcff6e7a
Clément Baratin <bobbot.contact@gmail.com>
2022-03-20 23:03:01 +0100

    delete module

--- src/generator/minimap.py
+++ src/generator/minimap.py
@@ -1,11 +1,9 @@
-from email.encoders import encode_noop
 import json 
-
 
 class Minimap :
 
     def __init__(self, name : str) -> None :
-        with open(f"ressources/map/{name}.tmj", "r", encoding='utf8') as r_file :
+        with open(f"assets/map/{name}.tmj", "r", encoding='utf8') as r_file :
             r_file = json.load(r_file)
         self.raw = r_file
         self.door = {}
