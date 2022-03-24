ceb2d3553c49524d5bef015292749d194190538a
Clément Baratin <bobbot.contact@gmail.com>
2022-03-24 14:16:45 +0100

    Start to work on Door

--- src/tmx/generator/door.py
+++ src/tmx/generator/door.py
@@ -1,4 +1,14 @@
+from ...consts import Facing
+
 class Door:
     
-    def __init__(self):
-        pass
+    def __init__(self, name : str):
+        self.name = name 
+        if "north" in self.name :
+            self.orientation = Facing.NORTH
+        elif "east" in self.name :
+            self.orientation = Facing.EAST
+        elif "south" in self.name :
+            self.orientation = Facing.SOUTH
+        elif "west" in self.name :
+            self.orientation = Facing.WEST
