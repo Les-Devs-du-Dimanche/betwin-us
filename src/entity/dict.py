e7feb076940bdc47e70d4d2079c223ce0e96dcd8
Clément Baratin <bobbot.contact@gmail.com>
2022-03-25 22:52:33 +0100

    Add two mobs, new room

--- src/entity/dict.py
+++ src/entity/dict.py
@@ -1,5 +1,9 @@
+from .enemy.cursed_chest import CursedChest
 from .enemy.slime import Slime
+from .enemy.firesoul import Firesoul
 
 entity_dict = {
     'slime': Slime,
+    'firesoul' : Firesoul, 
+    'cursed_chest' : CursedChest,    
 }
